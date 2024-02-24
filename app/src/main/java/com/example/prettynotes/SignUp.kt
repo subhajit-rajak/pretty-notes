4b60bf90ee6798f1ba9fedd80b0b71cbcd233361
Subhajit Rajak <subhajitrajak2001@gmail.com>
2024-02-24 05:50:29 +0530

    Added Feature - Login and Sign Up Screen

--- app/src/main/java/com/example/prettynotes/SignUp.kt
+++ app/src/main/java/com/example/prettynotes/SignUp.kt
@@ -3,19 +3,18 @@ package com.example.prettynotes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.prettynotes.databinding.ActivityLoginBinding
 import com.example.prettynotes.databinding.ActivitySignUpBinding
 
-class Login : AppCompatActivity() {
-    private val binding: ActivityLoginBinding by lazy {
-        ActivityLoginBinding.inflate(layoutInflater)
+class SignUp : AppCompatActivity() {
+    private val binding: ActivitySignUpBinding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.signup.setOnClickListener {
-            startActivity(Intent(this, SignUp::class.java))
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
             finish()
         }
     }
