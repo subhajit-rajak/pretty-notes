b8b321daef0658068496156f66923bfa9f1fed41
Subhajit Rajak <subhajitrajak2001@gmail.com>
2024-02-25 05:48:23 +0530

    Added Feature - Google Login & Sign-out

--- app/src/main/java/com/example/prettynotes/Login.kt
+++ app/src/main/java/com/example/prettynotes/Login.kt
@@ -1,19 +1,28 @@
 package com.example.prettynotes
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.prettynotes.databinding.ActivityLoginBinding
-import com.example.prettynotes.databinding.ActivitySignUpBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class Login : AppCompatActivity() {
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
+
+    // need to comment this out if signing out doesn't work and still stays on MainActivity
     override fun onStart() {
         super.onStart()
 
@@ -24,9 +33,16 @@ class Login : AppCompatActivity() {
             finish()
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient= GoogleSignIn.getClient(this, gso)
 
         //initialize firebase auth
         auth=FirebaseAuth.getInstance()
@@ -54,6 +70,31 @@ class Login : AppCompatActivity() {
                         }
                     }
             }
+        }
+
+        binding.google.setOnClickListener() {
+            val signInClient=googleSignInClient.signInIntent
+            launcher.launch(signInClient)
+        }
+    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result->
+        if(result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            if(task.isSuccessful) {
+                val account:GoogleSignInAccount?=task.result
+                val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+                auth.signInWithCredential(credential).addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                    } else {
+                        Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        } else {
+            Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/prettynotes/MainActivity.kt
+++ app/src/main/java/com/example/prettynotes/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.prettynotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.prettynotes.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.logout.setOnClickListener {
+            // for sign-out
+
+            // option 1
+            FirebaseAuth.getInstance().signOut()
+
+            // option 2
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val go= GoogleSignIn.getClient(this, gso)
+            go.signOut()
+
+            // both options were needed to sign-out properly
+
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }
     }
 }
